bea68981afcc451a6a780ae9cd4723b4422e3719
fznakbar <fauzanakbar10@gmail.com>
2020-03-10 21:23:29 +0700

    added kanban home page

--- client/index.js
+++ client/index.js
@@ -1,14 +1,15 @@
 const urlHeroku = "https://murmuring-lowlands-78173.herokuapp.com/"
+const urlLocalHost = "http://localhost:3000/"
 
-$(document).ready(
-    $.ajax({
-        method : 'get',
-        url : urlHeroku,
-        success : (data)=>{
-            $('#hello-world').append(`${data}`)
-        },
-        error : (err)=>{
-            console.log(err)
-        }
-    })
-)
+// $(document).ready(
+//     $.ajax({
+//         method : 'get',
+//         url : urlLocalHost,
+//         success : (data)=>{
+//             $('#hello-world').append(`${data}`)
+//         },
+//         error : (err)=>{
+//             console.log(err)
+//         }
+//     })
+// )
